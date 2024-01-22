4e85cc70adb55c278bc795a205b0dde3a7f7b3d7
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2024-01-22 15:49:41 +0100

    wip: refactor cv

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,6 @@ fn main() {
   let author = get_author_name().expect("Failed to get author name");
   let cv_repo = get_cv_repo();
   let cv_version = get_cv_version();
-  let filename = format!("CV {} {}.pdf", author, dt.format("%B %Y"));
 
   with_files_included!(
     "work---2004-apr--2005-mar---atlas.tex",
@@ -74,29 +73,39 @@ fn main() {
     "work---2007-oct--2016-jul---ps.tex",
     "work---2016-aug--2019-apr---veon.tex",
     "work---2019-may--2020-jul---stackstate.tex",
-    "work---2019-nov--2020-jul---stackstate.tex",
     "work---2020-aug--2021-aug---ing.tex",
     "work---2021-aug--2023-dec---klm.tex",
+    "personal-projects-content.tex",
     "personal-projects.tex",
     "opensource.tex",
+    "startup-content.tex",
     "startup.tex",
+    "startup-and-personal-projects.tex",
     "skills.tex",
     "certs.tex",
     "edu.tex",
     "work.tex",
-    "cv.tex",
+    "cv-for-e.tex",
+    "cv-for-s.tex",
     "me.jpg"; {
       resize_image(0.40);
-      
-      let pdf_data: Vec<u8> = tectonic::latex_to_pdf(format!(r#"
-        \newcommand{{\cvdate}}{{{}}}
-        \newcommand{{\cvrepo}}{{{}}}
-        \newcommand{{\cvversion}}{{{}}}
-        \input{{cv}}
-      "#, dt.format("%B, %Y"), cv_repo, cv_version)).expect("Processing failed");
-      let mut file = fs::File::create(&filename)
-          .expect(&format!("Could not create {}", &filename));
-      file.write_all(&pdf_data).expect(&format!("Could not write to {}", &filename));
-      println!("{} bytes saved to \"{}\"", pdf_data.len(), filename);
+
+      for suffix in [
+        'e',
+        // 's',
+        ] {
+        let filename = format!("CV {} {} {}.pdf", author, dt.format("%B %Y"), suffix);
+
+        let pdf_data: Vec<u8> = tectonic::latex_to_pdf(format!(r#"
+          \newcommand{{\cvdate}}{{{}}}
+          \newcommand{{\cvrepo}}{{{}}}
+          \newcommand{{\cvversion}}{{{}}}
+          \input{{cv-for-{}}}
+        "#, dt.format("%B, %Y"), cv_repo, cv_version, suffix)).expect("Processing failed");
+        let mut file = fs::File::create(&filename)
+            .expect(&format!("Could not create {}", &filename));
+        file.write_all(&pdf_data).expect(&format!("Could not write to {}", &filename));
+        println!("{} bytes saved to \"{}\"", pdf_data.len(), filename);
+      }
   });
 }
